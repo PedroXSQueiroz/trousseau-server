f1033e4579316aaed5cffa627e2cf286f309506e
PedroXSQueiroz <pedroxsqueiroz@gmail.com>
2021-01-24 18:20:56 -0300

    Adding fail indicators

---
+++ src/main/java/br/com/pedroxsqueiroz/trousseau_server/contants/enums/TrousseauFailEnum.java
@@ -0,0 +1,9 @@
+package br.com.pedroxsqueiroz.trousseau_server.contants.enums;
+
+public enum TrousseauFailEnum {
+
+    MISPLACED,
+    INVALIDATED_BY_USER,
+    INVALIDATED_BY_AUTOCHECK
+
+}
---
+++ src/main/resources/db/migration/V005__adding_trousseau_fail_indicator.sql
@@ -0,0 +1,2 @@
+ALTER TABLE trousseau
+    ADD trousseau_fail VARCHAR(50);
