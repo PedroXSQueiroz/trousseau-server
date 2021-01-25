93b3858b5b2d9a7fba908608c3d4203a2bf85de0
PedroXSQueiroz <pedroxsqueiroz@gmail.com>
2021-01-24 21:01:15 -0300

    Menor changes to merge

--- src/main/java/br/com/pedroxsqueiroz/trousseau_server/repositories/ItemDao.java
+++ src/main/java/br/com/pedroxsqueiroz/trousseau_server/repositories/ItemDao.java
@@ -1,9 +1,12 @@
 package br.com.pedroxsqueiroz.trousseau_server.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import br.com.pedroxsqueiroz.trousseau_server.models.Flat;
 import br.com.pedroxsqueiroz.trousseau_server.models.Item;
 
 @Repository
